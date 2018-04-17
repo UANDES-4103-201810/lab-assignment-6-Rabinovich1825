1a6c33cfb9be1f4155aff4b8fdeb00cce4a6e34f
Benjamin Perez <baperez@miuandes.cl>
2018-04-17 14:38:06 -0300

    user creation ready

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,20 @@
 class RegistrationsController < ApplicationController
 	def new
-	
+
+	end
+
+	def registrations_params
+		params.require(:registrations).permit(:name, :last_name, :email, :password, :phone)
 	end
 
 	def create
-	    #complete this method
+		user = User.create(registrations_params)
+		if user.valid?
+			redirect_to root_path
+		else
+			puts user.errors.messages
+			redirect_to registrations_path
+		end
 	end
+
 end
