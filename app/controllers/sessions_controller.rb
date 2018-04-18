9c0d039f68b54cb368a39522e58012e915866152
Benjamin Perez <baperez@miuandes.cl>
2018-04-17 22:37:38 -0300

    bug fixed: password verified

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,10 +9,15 @@ class SessionsController < ApplicationController
 	def create
 		user = User.find_by_email(session_params[:email])
 		if user != nil
-			session[:current_user_id] = user.id
-      session[:logged_in] = true
-      flash[:notice] = "Logged in!"
-			redirect_to :controller => 'users', :action => 'show', :id => user.id
+			if session_params[:password] == user.password
+				session[:current_user_id] = user.id
+				session[:logged_in] = true
+				flash[:notice] = "Logged in!"
+				redirect_to :controller => 'users', :action => 'show', :id => user.id
+			end
+			flash[:notice] = "Log in failed!"
+			session[:logged_in] = false
+			redirect_to log_in_path
 		else
 			flash[:notice] = "Log in failed!"
       session[:logged_in] = false
